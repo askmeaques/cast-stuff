0727b656427ad0b904b6594a58c5502952ba4bd7
Amy Kruger <amy.kruger@castandcrew.com>
2019-01-29 13:27:47 -0800

    once again

---
+++ available-vols.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Script uses AWS CLI to search all regions for volumes in the available state
+
+
+for regions in `aws ec2 describe-regions --output text|awk {'print $3'}`; 
+do    
+for volumes in `aws ec2 describe-volumes --region $regions --output text| grep available | awk '{print $9}' | grep vol| tr '\n' ' '`;
+do   echo $regions" "$volumes; 
+done; 
+done
+
